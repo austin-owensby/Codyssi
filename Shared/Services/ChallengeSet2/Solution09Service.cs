0d79e7b96372a8f93587b49e0811f5b7df871537
Austin Owensby <austin_owensby@hotmail.com>
2025-03-21 18:11:23 -0500

    Day 9 solution

--- Shared/Services/ChallengeSet2/Solution09Service.cs
+++ Shared/Services/ChallengeSet2/Solution09Service.cs
@@ -1,19 +1,19 @@
 namespace Codyssi.Services
 {
     // (ctrl/command + click) the link to open the input file
-    // file://./../../Inputs/09.txt
+    // file://./../../../Inputs/09.txt
     public class Solution09Service : ISolutionDayService
     {
         public string RunPart1Solution(bool example)
         {
             List<string> lines = FileUtility.GetInputLines(9, example);
+            List<Point> points = lines.QuickRegex(@"\((-?\d+), (-?\d+)\)").Select(x => x.ToInts()).Select(x => new Point(x[0], x[1])).ToList();
+            List<int> distances = points.Select(Utility.ManhattanDistance).ToList();
 
-            int answer = 0;
+            int min = distances.Min();
+            int max = distances.Max();
 
-            foreach (string line in lines)
-            {
-                
-            }
+            int answer = max - min;
 
             return answer.ToString();
         }
@@ -21,13 +21,12 @@ namespace Codyssi.Services
         public string RunPart2Solution(bool example)
         {
             List<string> lines = FileUtility.GetInputLines(9, example);
+            List<Point> points = lines.QuickRegex(@"\((-?\d+), (-?\d+)\)").Select(x => x.ToInts()).Select(x => new Point(x[0], x[1])).ToList();
 
-            int answer = 0;
+            Point closestPoint = points.OrderBy(Utility.ManhattanDistance).ThenBy(p => p.X).ThenBy(p => p.Y).First();
+            points.Remove(closestPoint);
 
-            foreach (string line in lines)
-            {
-                
-            }
+            int answer = points.Min(point => Utility.ManhattanDistance(point, closestPoint));
 
             return answer.ToString();
         }
@@ -35,12 +34,18 @@ namespace Codyssi.Services
         public string RunPart3Solution(bool example)
         {
             List<string> lines = FileUtility.GetInputLines(9, example);
+            List<Point> points = lines.QuickRegex(@"\((-?\d+), (-?\d+)\)").Select(x => x.ToInts()).Select(x => new Point(x[0], x[1])).ToList();
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            Point currentPoint = new();
 
+            while (points.Count > 0) {
+                Point closestPoint = points.OrderBy(point => Utility.ManhattanDistance(point, currentPoint)).ThenBy(p => p.X).ThenBy(p => p.Y).First();
+                points.Remove(closestPoint);
+
+                answer += Utility.ManhattanDistance(currentPoint, closestPoint);
+                currentPoint = closestPoint;
             }
 
             return answer.ToString();
