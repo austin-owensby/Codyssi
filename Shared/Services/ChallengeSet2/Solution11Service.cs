ae19485b35464156a2a0c181f43cc2b275f00ed8
Austin Owensby <austin_owensby@hotmail.com>
2025-03-24 00:10:50 -0500

    Day 11

--- Shared/Services/ChallengeSet2/Solution11Service.cs
+++ Shared/Services/ChallengeSet2/Solution11Service.cs
@@ -1,19 +1,24 @@
 namespace Codyssi.Services
 {
     // (ctrl/command + click) the link to open the input file
-    // file://./../../Inputs/11.txt
+    // file://./../../../Inputs/11.txt
     public class Solution11Service : ISolutionDayService
     {
         public string RunPart1Solution(bool example)
         {
             List<string> lines = FileUtility.GetInputLines(11, example);
+            List<List<string>> parts = lines.ChunkByExclusive(string.IsNullOrWhiteSpace);
 
-            int answer = 0;
+            List<int> frequencies = parts[0].ToInts();
+            List<(int, int)> swaps = parts[1].Select(p => p.Split('-').ToInts()).Select(x => (x[0], x[1])).ToList();
+            int testIndex = int.Parse(parts[2][0]);
 
-            foreach (string line in lines)
+            foreach ((int, int) swap in swaps)
             {
-                
+                (frequencies[swap.Item2 - 1], frequencies[swap.Item1 - 1]) = (frequencies[swap.Item1 - 1], frequencies[swap.Item2 - 1]);
             }
+
+            int answer = frequencies[testIndex - 1];
 
             return answer.ToString();
         }
@@ -21,13 +26,26 @@ namespace Codyssi.Services
         public string RunPart2Solution(bool example)
         {
             List<string> lines = FileUtility.GetInputLines(11, example);
+            List<List<string>> parts = lines.ChunkByExclusive(string.IsNullOrWhiteSpace);
 
-            int answer = 0;
+            List<int> frequencies = parts[0].ToInts();
 
-            foreach (string line in lines)
-            {
-                
+            List<int> swapDigits = parts[1].Select(p => p.Split('-').ToInts()).SelectMany().ToList();
+            swapDigits.Add(swapDigits[0]);
+            List<(int, int, int)> swaps = [];
+
+            for (int i = 0; i < swapDigits.Count - 2; i += 2) {
+                swaps.Add((swapDigits[i], swapDigits[i + 1], swapDigits[i + 2]));
             }
+
+            int testIndex = int.Parse(parts[2][0]);
+
+            foreach ((int, int, int) swap in swaps)
+            {
+                (frequencies[swap.Item2 - 1], frequencies[swap.Item3 - 1], frequencies[swap.Item1 - 1]) = (frequencies[swap.Item1 - 1], frequencies[swap.Item2 - 1], frequencies[swap.Item3 - 1]);
+            }
+
+            int answer = frequencies[testIndex - 1];
 
             return answer.ToString();
         }
@@ -35,13 +53,23 @@ namespace Codyssi.Services
         public string RunPart3Solution(bool example)
         {
             List<string> lines = FileUtility.GetInputLines(11, example);
+            List<List<string>> parts = lines.ChunkByExclusive(string.IsNullOrWhiteSpace);
 
-            int answer = 0;
+            List<int> frequencies = parts[0].ToInts();
+            List<(int, int)> swaps = parts[1].Select(p => p.Split('-').ToInts().Order().ToList()).Select(x => (x[0], x[1])).ToList();
+            int testIndex = int.Parse(parts[2][0]);
 
-            foreach (string line in lines)
+            foreach ((int, int) swap in swaps)
             {
+                int blockSize = Math.Min(swap.Item2 - swap.Item1, frequencies.Count - swap.Item2 + 1);
+
+                foreach (int i in blockSize) {
+                    (frequencies[swap.Item2 - 1 + i], frequencies[swap.Item1 - 1 + i]) = (frequencies[swap.Item1 - 1 + i], frequencies[swap.Item2 - 1 + i]);
+                }
 
             }
+
+            int answer = frequencies[testIndex - 1];
 
             return answer.ToString();
         }
